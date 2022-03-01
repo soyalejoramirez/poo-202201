099bcea5843162970d3d127c84f37537e48d8043
Alejandro Ramírez <alejandroramirez@saimyr.com.co>
2022-02-28 19:48:24 -0500

    Iniciamos con las listas.

---
+++ src/main/java/com/agenda/dominio/Agenda.java
@@ -0,0 +1,23 @@
+package com.agenda.dominio;
+
+import java.util.List;
+
+public class Agenda {
+    public List<Contacto> contactos;
+
+    public void agregar(Contacto contactoAAgregar) {
+
+    }
+
+    public void eliminar(Contacto contactoAEliminar) {
+
+    }
+
+    public void editar(Contacto contactoAEditar) {
+
+    }
+
+    public Contacto buscar(long celular) {
+        return null;
+    }
+}
---
+++ src/main/java/com/agenda/dominio/Contacto.java
@@ -0,0 +1,13 @@
+package com.agenda.dominio;
+
+public class Contacto {
+    public String nombre;
+    public String apellido;
+    public long celular;
+
+    public Contacto(String nombre, String apellido, long celular) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.celular = celular;
+    }
+}
