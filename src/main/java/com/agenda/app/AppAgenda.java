846d8dcd2d5cb065bb60b18643bc0eaa375b65cd
Alejandro Ramírez <alejandroramirez@saimyr.com.co>
2022-03-14 19:00:00 -0500

    Se trabaja en el punto de agenda del taller.

--- src/main/java/com/agenda/app/AppAgenda.java
+++ src/main/java/com/agenda/app/AppAgenda.java
@@ -10,11 +10,12 @@ public class AppAgenda {
 
         agenda.agregar("Georgina", "Ronaldo", 3113776458l);
         agenda.agregar("Ana", "Bedoya", 3219000174l);
+        agenda.agregar("Berenice", "Zuluaga", 3219316174l);
         agenda.agregar("Carlos", "Mattos", 3002587496l);
+        agenda.agregar("Carlos", "Maderos", 3022587496l);
         agenda.agregar("Alejandro", "Ramirez", 3104698710l);
 
-        agenda.imprimirAgenda();
-        Contacto c = agenda.buscarConFilter(3104698710l);
+        Contacto c = agenda.buscarPorCelularConFilter(3104698710l);
 
         if (c != null) {
             System.out.println("Contacto buscado: " + c.getNombre());
@@ -22,9 +23,9 @@ public class AppAgenda {
             System.out.println("Contacto no encontrado");
         }
 
-        c.setCelular(3110008796l);
-        agenda.editar(c);
+        agenda.editar(3104698710l, 3110008796l);
 
-        agenda.imprimirAgenda();
+        System.out.println("----- CONTACTOS ORDENADOS ----- ");
+        agenda.ordenarAlfabeticamente();
     }
 }
--- src/main/java/com/agenda/dominio/Agenda.java
+++ src/main/java/com/agenda/dominio/Agenda.java
@@ -1,8 +1,10 @@
 package com.agenda.dominio;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class Agenda {
     private List<Contacto> contactos;
@@ -16,32 +18,47 @@ public class Agenda {
         this.contactos.add(contactoAAgregar);
     }
 
-    public void eliminar(Contacto contactoAEliminar) {
-        boolean eliminoCorrectamente = this.contactos.remove(contactoAEliminar);
+    public void eliminar(long celular) {
+        Contacto contactoAEliminar = this.buscarPorCelularConFilter(celular);
 
-        if (eliminoCorrectamente) {
+        if (contactoAEliminar != null) {
+            this.contactos.remove(contactoAEliminar);
             System.out.println("Se eliminó el contacto " + contactoAEliminar.getNombre());
         } else {
             System.out.println("No se eliminó nada. Porque el contacto " + contactoAEliminar.getNombre() + " no existe.");
         }
     }
 
-    public void editar(Contacto contactoAEditar) {
-        Contacto c = buscarConFilter(contactoAEditar.getCelular());
+    public Contacto buscarPorCelularConFilter(long celular) {
+        Contacto contactoBuscado = this.contactos.stream()
+                .filter(contacto -> contacto.getCelular() == celular)
+                .findFirst().orElse(null);
+        return contactoBuscado;
+    }
+
+    public List<Contacto> buscarPorNombre(String nombre) {
+        return null;
+    }
+
+    public List<Contacto> buscarPorApellido(String apellido) {
+        return null;
+    }
+
+    public void editar(long celularViejo, long celularNuevo) {
+        Contacto c = buscarPorCelularConFilter(celularViejo);
 
         if(c != null) {
             int posicionAReemplazar = this.contactos.indexOf(c);
-            this.contactos.set(posicionAReemplazar, contactoAEditar);
+            c.setCelular(celularNuevo);
+            this.contactos.set(posicionAReemplazar, c);
         } else {
             System.out.println("El contacto a editar no existe.");
         }
     }
 
-    public Contacto buscarConFilter(long celular) {
-        Contacto contactoBuscado = this.contactos.stream()
-                                    .filter(contacto -> contacto.getCelular() == celular)
-                                    .findFirst().orElse(null);
-        return contactoBuscado;
+    public void ordenarAlfabeticamente() {
+        this.contactos.sort(Comparator.comparing(Contacto::getNombre).thenComparing(Contacto::getApellido));
+        this.imprimirAgenda();
     }
 
     public Contacto buscarConForEach(long celular) {
@@ -55,11 +72,8 @@ public class Agenda {
         return contactoBuscado.get();
     }
 
-    public void imprimirAgenda() {
-        this.contactos.forEach(c -> System.out.println(c.getNombre() + " - " + c.getCelular()));
-    }
-
-    public void imprimirAgendaOrdenada() {
+    private void imprimirAgenda() {
+        this.contactos.forEach(c -> System.out.println(c.getNombre() + " " + c.getApellido() + " - " + c.getCelular()));
     }
 
     public List<Contacto> getContactos() {
